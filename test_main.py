22fb0fcb344eb6045c946f5b94e56ab3a4e4886a
Sowsree13 <145466086+Sowsree13@users.noreply.github.com>
2024-10-13 14:24:33 +0530

    Create test_main.py

---
+++ test_main.py
@@ -0,0 +1,13 @@
+import unittest
+from main import hello,bye
+
+class TestMain(unittest.TestCase):
+  def test_hello(self):
+    self.assertEqual(hello(), "hi")
+
+
+  def test_bye(self):
+    self.assertEqual(bye(), "bye")
+
+if __name__ == "__main__":
+  unittest.main()
