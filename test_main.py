782b2bb125d2847d1758c6f200db4c09f2fa344a
Sowsree13 <145466086+Sowsree13@users.noreply.github.com>
2024-10-13 15:11:20 +0530

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -6,9 +6,11 @@ from main import bye, hello
 class TestMain(unittest.TestCase):
     def test_hello(self):
         self.assertEqual(hello(), "hi")
+        return "hi"
 
     def test_bye(self):
         self.assertEqual(bye(), "bye")
+        return "bye"
 
 
 if __name__ == "__main__":
